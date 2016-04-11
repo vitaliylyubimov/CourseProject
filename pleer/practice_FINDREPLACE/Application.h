7b7f926f7bf5cd69468bc6c1fcaff9b93240b912
vitaliylyubimov <l0g_d0g@bk.ru>
2016-04-11 12:30:38 +0300

    Init project

---
+++ pleer/practice_FINDREPLACE/Application.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "include.h"
+#include "DlgPlayList.h"
+#include "DlgEqualizer.h"
+class Application
+{
+private:
+	DlgPlayList dlg;								//Class object PlayList
+	DlgEqualizer equalizer;							//Class object Equalizer
+	HWND hTBSoundVolume;							//TRackBar - volume 
+	HWND hTBPlayingSong;							//TRackBAr - current time songs
+	UINT id_timer;									//ID timer time by current playing songs
+	UINT idTimerBySpectr;							//ID timer by Spectr
+	INT secPlaying;									//Count of seconds played of the current song
+	HWND spectrs[44];								//Columns spectrs
+	HICON hIcon;									//Icon pleer
+	BOOL IsRepeatSong;								//Replay
+public:
+	HSTREAM hStream;								//Поток воспроизведения песни
+	Application(VOID);							//конструктор по умолчанию
+	~Application();								//деструктор
+	static Application* _this;					//собственный указатель
+	static INT_PTR CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);				//Диалоговая процедура для диалогового окна
+	
+	VOID Cls_OnCommand(HWND hwnd, INT id, HWND hwndCtl, UINT codeNotify);								//WM_COMMAND
+	BOOL Cls_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam);									//WM_INITDIALOG
+	VOID Cls_OnHScroll(HWND hwnd, HWND hwndCtl, UINT code, INT pos);									//WM_HSCROLL
+	VOID Cls_OnTimer(HWND hwnd, UINT id);
+	VOID Cls_OnSysCommand(HWND hwnd, UINT cmd, INT x, INT y);
+	
+	INT runProgramm();										//старт программы
+	INT CheckedInitBASS();									//проверка инициализации BASS
+	BOOL openFile_LoadMusic(HWND hWnd);						//открытие песни с компьютера OPENFILENAME
+	VOID setRangeTrackBarPlaySong(HSTREAM stream);
+
+	VOID stop(HSTREAM stream);		//Stop
+	VOID play(HSTREAM stream);		//Play
+	VOID pause();					//Pause
+	VOID next();					//Next Song
+	VOID prev();					//Prev Song
+
+	VOID ControlSpectr(HWND hWnd);				//Контроль звуковых частот при проигрывании
+	VOID SetSpectrColor(HWND hWnd);				//Установка цвета визуализации
+	VOID SetNullPosSpectr();					//Установка визуализации на 0 (При нажатии на Паузу/Стоп)
+		
+	VOID DeleteIconOfTray(HWND hWnd);			//Разворачивание окна из трея
+	VOID AddIconInTray(HWND hWnd);				//Сворачивание окна в Трей
+
+	friend class DlgPlayList;
+};
